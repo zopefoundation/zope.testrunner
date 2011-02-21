bcdbbe0489cbda19c0b31f38dd263bc645f7e54f
Michael Howitz <mh@gocept.com>
2011-02-21 12:23:44 +0000

    Back to development: 4.0.2

--- setup.py
+++ setup.py
@@ -145,7 +145,7 @@ long_description=(
 
 setup(
     name='zope.testrunner',
-    version='4.0.1',
+    version='4.0.2dev',
     url='http://pypi.python.org/pypi/zope.testrunner',
     license='ZPL 2.1',
     description='Zope testrunner script.',
