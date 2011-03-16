62d668a8a9a3d46378278a23b43d64fb92383c04
Lennart Regebro <regebro@gmail.com>
2011-03-16 15:27:02 +0000

    Preparing release 4.0.2

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ long_description=(
 
 setup(
     name='zope.testrunner',
-    version='4.0.2dev',
+    version='4.0.2',
     url='http://pypi.python.org/pypi/zope.testrunner',
     license='ZPL 2.1',
     description='Zope testrunner script.',
