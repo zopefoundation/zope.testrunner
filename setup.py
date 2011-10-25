024e814bd8d0fd2fd1f0db246da7979492f15c7f
Tres Seaver <tseaver@palladion.com>
2011-10-25 17:21:01 +0000

    Tag 4.0.4 release.

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ long_description=(
 
 setup(
     name='zope.testrunner',
-    version='4.0.4dev',
+    version='4.0.4',
     url='http://pypi.python.org/pypi/zope.testrunner',
     license='ZPL 2.1',
     description='Zope testrunner script.',
