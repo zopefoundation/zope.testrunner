9f4adbd76b348b58173860e127d9a5d9f56efb71
Marius Gedminas <marius@gedmin.as>
2013-02-08 13:43:16 +0000

    Back to development: 4.1.2

--- setup.py
+++ setup.py
@@ -157,7 +157,7 @@ long_description=(
 
 setup(
     name='zope.testrunner',
-    version='4.1.1',
+    version='4.1.2.dev0',
     url='http://pypi.python.org/pypi/zope.testrunner',
     license='ZPL 2.1',
     description='Zope testrunner script.',
