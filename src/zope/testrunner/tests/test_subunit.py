f82d5ce67d37f374feabebd483d800bfabd83134
Tres Seaver <tseaver@palladion.com>
2013-04-09 17:38:26 -0400

    Make tests pass again when subunit can't be imported.

--- src/zope/testrunner/tests/test_subunit.py
+++ src/zope/testrunner/tests/test_subunit.py
@@ -28,42 +28,53 @@ except NameError:
     unichr = chr # Python 3
 
 
-class TestSubunitTracebackPrinting(unittest.TestCase):
+try:
+    import subunit
+except ImportError:
+    def test_suite():
+        return unittest.TestSuite()
+else:
 
-    def makeByteStringFailure(self, text, encoding):
-        try:
-            # Note that this deliberately throws a string of bytes instead
-            # of a unicode object.  This simulates errors thrown by
-            # utf8-encoded doctests.
-            bytestr = text.encode(encoding)
-            self.fail(bytestr)
-        except self.failureException:
-            return sys.exc_info()
+    class TestSubunitTracebackPrinting(unittest.TestCase):
 
-    def setUp(self):
-        class FormatterOptions:
-            verbose=False
-        options = FormatterOptions()
+        def makeByteStringFailure(self, text, encoding):
+            try:
+                # Note that this deliberately throws a string of bytes instead
+                # of a unicode object.  This simulates errors thrown by
+                # utf8-encoded doctests.
+                bytestr = text.encode(encoding)
+                self.fail(bytestr)
+            except self.failureException:
+                return sys.exc_info()
 
-        self.output = StringIO()
-        self.subunit_formatter = formatter.SubunitOutputFormatter(
-            options, stream=self.output)
+        def setUp(self):
+            class FormatterOptions:
+                verbose=False
+            options = FormatterOptions()
 
-    def test_print_failure_containing_utf8_bytestrings(self):
-        exc_info = self.makeByteStringFailure(unichr(6514), 'utf8')
-        self.subunit_formatter.test_failure(self, 0, exc_info)
-        assert "AssertionError: \xe1\xa5\xb2" in self.output.getvalue()
-        # '\xe1\xa5\xb2'.decode('utf-8') == unichr(6514)
+            self.output = StringIO()
+            self.subunit_formatter = formatter.SubunitOutputFormatter(
+                options, stream=self.output)
 
-    def test_print_error_containing_utf8_bytestrings(self):
-        exc_info = self.makeByteStringFailure(unichr(6514), 'utf8')
-        self.subunit_formatter.test_error(self, 0, exc_info)
-        assert "AssertionError: \xe1\xa5\xb2" in self.output.getvalue()
-        # '\xe1\xa5\xb2'.decode('utf-8') == unichr(6514)
+        def test_print_failure_containing_utf8_bytestrings(self):
+            exc_info = self.makeByteStringFailure(unichr(6514), 'utf8')
+            self.subunit_formatter.test_failure(self, 0, exc_info)
+            assert "AssertionError: \xe1\xa5\xb2" in self.output.getvalue()
+            # '\xe1\xa5\xb2'.decode('utf-8') == unichr(6514)
 
-    def test_print_failure_containing_latin1_bytestrings(self):
-        exc_info = self.makeByteStringFailure(unichr(241), 'latin1')
-        self.subunit_formatter.test_failure(self, 0, exc_info)
-        assert "AssertionError: \xef\xbf\xbd" in self.output.getvalue()
-        # '\xef\xbf\xbd'.decode('utf-8') = unichr(0xFFFD)
+        def test_print_error_containing_utf8_bytestrings(self):
+            exc_info = self.makeByteStringFailure(unichr(6514), 'utf8')
+            self.subunit_formatter.test_error(self, 0, exc_info)
+            assert "AssertionError: \xe1\xa5\xb2" in self.output.getvalue()
+            # '\xe1\xa5\xb2'.decode('utf-8') == unichr(6514)
 
+        def test_print_failure_containing_latin1_bytestrings(self):
+            exc_info = self.makeByteStringFailure(unichr(241), 'latin1')
+            self.subunit_formatter.test_failure(self, 0, exc_info)
+            assert "AssertionError: \xef\xbf\xbd" in self.output.getvalue()
+            # '\xef\xbf\xbd'.decode('utf-8') = unichr(0xFFFD)
+
+    def test_suite():
+        return unittest.TestSuite((
+            unittest.makeSuite(TestSubunitTracebackPrinting),
+        ))
