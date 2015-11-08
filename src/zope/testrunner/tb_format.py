45b26f380f3ba07c7c1d12413e81c8f3f649279c
Felix Yan <felixonmars@archlinux.org>
2015-11-08 12:44:43 +0800

    Update _iter_chain to the latest code in Python 3.4

--- src/zope/testrunner/tb_format.py
+++ src/zope/testrunner/tb_format.py
@@ -29,15 +29,16 @@ except AttributeError:
             seen = set()
         seen.add(exc)
         its = []
+        context = exc.__context__
         cause = exc.__cause__
         if cause is not None and cause not in seen:
-            its.append(_iter_chain(cause, None, seen))
+            its.append(_iter_chain(cause, False, seen))
             its.append([(traceback._cause_message, None)])
-        else:
-            context = exc.__context__
-            if context is not None and context not in seen:
-                its.append(_iter_chain(context, None, seen))
-                its.append([(traceback._context_message, None)])
+        elif (context is not None and
+              not exc.__suppress_context__ and
+              context not in seen):
+            its.append(_iter_chain(context, None, seen))
+            its.append([(traceback._context_message, None)])
         its.append([(exc, custom_tb or exc.__traceback__)])
         # itertools.chain is in an extension module and may be unavailable
         for it in its:
